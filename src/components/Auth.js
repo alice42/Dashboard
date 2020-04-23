4d6b20b82a3375c0a86e4e6b5e26f2040f22bc06
mac <mac@macs-MacBook-Pro.local>
2020-04-23 15:05:28 +0200

    Simple auth component

---
+++ src/components/Auth.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+
+class Auth extends Component {
+  state = {
+    identifiant: '',
+    password: ''
+  }
+  handleSubmit = () => {
+    const { identifiant, password } = this.state
+    if (identifiant && password)
+      console.log('Submit Auth', identifiant, password)
+  }
+  handleChange = event => {
+    const { target } = event
+    const value = event.target.value
+    const { name } = target
+    this.setState({
+      [name]: value
+    })
+  }
+  render() {
+    return (
+      <div style={{ width: '100%', display: 'flex' }}>
+        <div>
+          identifiant
+          <input
+            type="text"
+            name="identifiant"
+            id="identifiant"
+            placeholder="identifiant"
+            value={this.state.identifiant}
+            onChange={e => {
+              this.handleChange(e)
+            }}
+          />
+          password
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="password"
+            value={this.state.password}
+            onChange={e => {
+              this.handleChange(e)
+            }}
+          />
+          <button onClick={() => this.handleSubmit()}>SUBMIT</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Auth
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import Auth from '../components/Auth'
 
 class App extends Component {
   render() {
-    return <div style={{ width: '100%', display: 'flex' }}>App ok</div>
+    return (
+      <div style={{ width: '100%', display: 'flex' }}>
+        <Auth />
+      </div>
+    )
   }
 }
 
