3c245c510588287216119200f575c7b64a4482a9
mac <mac@macs-MacBook-Pro.local>
2020-04-23 14:50:04 +0200

    Set up App
    React
    Redux
    Redux-Saga
    Webpack/Babel
    Prettier/EsLint
    EmotionsJS
    StoryBook
    Jest/ReactTestingLibrary

---
+++  Instructions.md
@@ -0,0 +1,10 @@
+# Contact
+
+## mail
+almartin@student.42.fr
+
+## phone
+
+0661602129
+
+# Code Explanation
---
+++ src/reducers/mainReducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux'
+
+const mainReducer = combineReducers({})
+
+export default mainReducer
